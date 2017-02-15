d544ce5451e2a1b34ad804fca6890d0927d34b2d
Chris Carr and Megan Warnock <megan@>
2017-02-14 16:33:34 -0800

    It will add rows and col.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 $(function() {
   $(".flashcard").click(function() {
-    $("body").removeClass();
     $(".initially-hidden").slideToggle();
   });
   $(".flashcard2").click(function() {
-    $("body").removeClass();
     $(".initially-hidden2").slideToggle();
   });
 });
